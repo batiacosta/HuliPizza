0e6118abe3a53bf05b5b33af67a3cf0b31d093a1
David Acosta <david.acosta.ing@gmail.com>
2025-07-07 11:08:55 -0500

    Setting up Scrollviews

--- HuliPizza/ContentView.swift
+++ HuliPizza/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    var orders:[Int] = []
+    var orders:[Int] = [1, 2, 3 ,4, 5]
     var body: some View {
         VStack {
             ZStack{
@@ -24,10 +24,14 @@ struct ContentView: View {
                     .font(.title)
                 Spacer()
             }
-            HStack(alignment: .firstTextBaseline) {
-                Text("Your order intem")
-                Spacer()
-                Text(19.90, format: .currency(code: "USD"))
+            ScrollView{
+                ForEach(orders, id: \.self) { order in
+                    HStack(alignment: .firstTextBaseline) {
+                        Text("Your order intem \(order)")
+                        Spacer()
+                        Text(19.90, format: .currency(code: "USD"))
+                    }
+                }
             }
             
             VStack {
@@ -36,11 +40,15 @@ struct ContentView: View {
                 Text("Margharita")
             }
             
-            HStack(alignment: .top, spacing: 15){
-                Image(systemName: "1.circle.fill").font(.largeTitle)
-                VStack (alignment: .leading){
-                    Text("Description ")
-                    Text("Margharita")
+            ScrollView {
+                ForEach(1 ... 25, id: \.self){ item in
+                    HStack(alignment: .top, spacing: 15){
+                        Image(systemName: "1.circle.fill").font(.largeTitle)
+                        VStack (alignment: .leading){
+                            Text("Description ")
+                            Text("Margharita")
+                        }
+                    }
                 }
             }
             Spacer()
