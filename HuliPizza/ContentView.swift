295b6808d4666a6c14b3be5291bbd65fcfbdf8cf
David Acosta <david.acosta.ing@gmail.com>
2025-07-14 11:54:27 -0500

    Adding button to switch views

--- HuliPizza/ContentView.swift
+++ HuliPizza/ContentView.swift
@@ -9,15 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var orders:[Int] = [1, 2, 3 ,4, 5]
-    var showOrders: Bool = false
+    @State private var showOrders: Bool = false
     var body: some View {
         VStack {
             HeaderView()
+                .shadow(radius: 5)
+            
+            Button{
+                showOrders.toggle()
+            } label: {
+                Image(systemName: showOrders ? "cart" : "menucard")
+                    .font(.title2)
+            }
             if showOrders {
                 OrderView(orders: orders)
                     .cornerRadius(10)
             }else{
                 MenuItemView()
+                    .cornerRadius(10)
+                    .shadow(radius: 5)
                 MenuView()
             }
             Spacer()
