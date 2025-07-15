005f330bc605bbc618cc45b74459a70548402516
David Acosta <david.acosta.ing@gmail.com>
2025-07-15 12:17:55 -0500

    Adding TextField, and Text editor to the OrderView

--- HuliPizza/OrderItemView.swift
+++ HuliPizza/OrderItemView.swift
@@ -12,6 +12,8 @@ struct OrderItemView: View {
     @State private var quantity = 1
     @State private var doubleIngredient = false
     @State var pizzaCrust: PizzaCrust
+    @State private var name: String = ""
+    @State private var comment: String = ""
     
     init(orderItem: Binding<OrderItem>){
         self._orderItem = orderItem
@@ -19,11 +21,13 @@ struct OrderItemView: View {
     }
     var body: some View {
         VStack {
+            TextField("Name", text: $name)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
             Toggle(isOn: $doubleIngredient){
                 Text("Double Ingredients \(doubleIngredient ? "Yes" : "No" )")
-                Stepper(value: $quantity, in: 1 ... 10) {
-                    Text("\(quantity) \(quantity == 1 ? "pizza" : "pizzas")")
-                }
+            }
+            Stepper(value: $quantity, in: 1 ... 10) {
+                Text("\(quantity) \(quantity == 1 ? "pizza" : "pizzas")")
             }
             Picker(selection: $pizzaCrust) {
                 ForEach(PizzaCrust.allCases, id: \.self){ crust in
@@ -35,8 +39,15 @@ struct OrderItemView: View {
             .pickerStyle(MenuPickerStyle())
             //.pickerStyle(SegmentedPickerStyle())
             //.pickerStyle(WheelPickerStyle())
+            VStack {
+                Text("Comments")
+                TextEditor(text: $comment)
+            }
+            .clipShape(RoundedRectangle(cornerRadius: 5 ))
+            .shadow(radius: 1)
             Spacer()
         }
+        .padding()
     }
 }
 
